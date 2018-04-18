ea7d8abf66ca6dcfbeef850792c4532d626d4e03
Ricardo Medeiros <ricardo.medeiros.costa@gmail.com>
2018-04-17 23:30:25 -0300

    feat(regex-mail-test): add tests that test the regex-mail-functions
    Closes #1,#2

--- test/main.c
+++ test/main.c
@@ -3,15 +3,22 @@
 #include "../lib/er.h"
 
 int main(void)  {
-  // Apenas para melhorar a visualizacao
   printf("\n");
   
   printf("Testando regex do parametro mail_from : ");
-  if (strcmp("usermane@mail.com", mail_from("--mail-from  username@mail.com")) == 0)
+  if (("usermane@mail.com" == mail_from("--mail-from username@gmail.com")) == 0)
     printf("OK");
   else 
-    printf("FALHA");
+    printf("FAIL");
 
-  // Apenas para melhorar a visualizacao
-  printf("\n\n");
+  printf("\n");  
+
+
+  printf("Testando regex do parametro mail_rcpt: ");
+  if (("usermane@mail.com" == mail_rcpt("--mail-rcpt username@gmail.com")) == 0)
+    printf("OK");
+  else 
+    printf("FAIL");
+
+  printf("\n");  
 }
