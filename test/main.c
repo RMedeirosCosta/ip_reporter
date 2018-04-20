2c77970d2e6f138de7fd432e303702199d14f089
Ricardo Medeiros <ricardo.medeiros.costa@gmail.com>
2018-04-20 00:38:58 -0300

    feat(password): create a feature to catch password argument
    Closes #6

--- test/main.c
+++ test/main.c
@@ -7,12 +7,16 @@ void print_message(char *message) {
 }
 
 int main(void) {
-  assert_equals(print_message,
+     assert_equals(print_message,
 		"Testing mail-from regex",		
 		"username@gmail.com",
 		mail_from("--mail-from username@gmail.com"));
     assert_equals(print_message,
 		"Testing mail-rcpt regex",		
 		"username@gmail.com",
-		mail_from("--mail-rcpt username@gmail.com"));
+		mail_rcpt("--mail-rcpt username@gmail.com"));
+    assert_equals(print_message,
+		  "Testing password regex",
+		  "username@gmail.com",
+		  password("--password 123@mudar"));
 }
