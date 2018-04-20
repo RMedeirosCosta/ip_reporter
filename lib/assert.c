11bf8afa728ac023988b0c269348339f9a563e60
Ricardo Medeiros <ricardo.medeiros.costa@gmail.com>
2018-04-20 00:30:20 -0300

    refactor(assert): create an assert lib to encapsulate the test complexity
    Closes #5

---
+++ lib/assert.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+int assert_equals(void (*printer)(char *), char *test_message, char *first_value, char *second_value) {
+  (*printer)(test_message);
+  if (strcmp(first_value, second_value) == 0) {
+    (*printer)("OK\n");
+    return 0;
+  }
+  else {
+    (*printer)("OK\n");   
+    return 1;
+  }  
+}
---
+++ lib/assert.h
@@ -0,0 +1,1 @@
+int assert_equals(void (*printer)(char *), char *test_message, char *first_value, char *second_value);
