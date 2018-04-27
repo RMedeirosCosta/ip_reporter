d1fc09f8b16d4574899376b0ea5f08bf75c6e15a
Ricardo Medeiros <ricardo.medeiros.costa@gmail.com>
2018-04-26 22:42:41 -0300

    chore(assert-result): add a space before assert result to aim improve the visualization

--- lib/assert.c
+++ lib/assert.c
@@ -3,11 +3,11 @@
 int assert_equals(void (*printer)(char *), char *test_message, char *first_value, char *second_value) {
   (*printer)(test_message);
   if (strcmp(first_value, second_value) == 0) {
-    (*printer)("OK\n");
+    (*printer)(" OK\n");
     return 0;
   }
   else {
-    (*printer)("OK\n");   
+    (*printer)(" OK\n");   
     return 1;
   }  
 }
